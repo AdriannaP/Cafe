d72f1f91a7211cb95cb9f3408b0284c287c3d73e
Ada Paczocha <ada-paczocha@o2.pl>
2020-05-30 17:49:18 +0200

    Home page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cafe.Interfaces;
+using Cafe.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+        public HomeController(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredCoffees = _coffeeRepository.PreferredCoffees
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Cafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafe.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Coffee> PreferredCoffees { get; set; }
+    }
+}
